6a695418fe61ef1fcf1d0d71e715144a4e600abf
MartinaZelli <martinazelli@live.it>
2023-12-09 23:02:45 +0100

    prima modifica

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        /*8. In un'aerodromo si vuole mantenere le informazioni sugli aeromobili.
+        Di ogni aeromobile si vuole sapere la sigla (string) che lo identifica.
+Gli alianti sono aeromobili caratterizzati da un numero (int) che ne descrive l'efficienza aereodinamica.
+Degli aerei a motore si vuole sapere la potenza in CV del motopropulsore (double).
+Aeromobili dello stesso tipo devono essere confrontabili fra loro in modo da poter stabilire quale sia quello con prestazioni migliori.
+Gli alianti si confrontano in base all'efficienza;
+gli aerei a motore in base alla potenza in CV.
+Vogliamo essere un grado di verificare se un aeromibile è superione adun altro come prestazioni.
+
+         */
     }
 }
